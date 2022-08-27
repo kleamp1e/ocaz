b1b8766cfd1e26ebfaa9c298006299416063fa7d
kleamp1e <kleamp1e@gmail.com>
2022-08-27 18:39:47 +0900

    顔検出するフレームを選択する処理を追加

--- ocaz/bin/detect_face.py
+++ ocaz/bin/detect_face.py
@@ -2,9 +2,11 @@
 
 import logging
 from typing import Dict
+import math
 
 import click
 import cv2
+import numpy as np
 
 
 class VideoCaptureOpener:
@@ -27,6 +29,25 @@ def get_video_info(video_capture) -> Dict:
         "n_frames": int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT)),
         "fps": video_capture.get(cv2.CAP_PROP_FPS),
     }
+
+
+def select_frames(
+    n_frames: int,
+    fps: float,
+    max_frames_per_second: float,
+    max_frames_per_video: int,
+):
+    duration = n_frames / fps
+    logging.debug("duration = %s", duration)
+    n_selected_frames = sorted(
+        [1, math.floor(duration * max_frames_per_second), max_frames_per_video]
+    )[1]
+    logging.debug("n_selected_frames = %s", n_selected_frames)
+    return np.unique(
+        np.linspace(
+            0, n_frames - 1, n_selected_frames + 1, endpoint=False, dtype=np.uint16
+        )[1:]
+    ).tolist()
 
 
 @click.command()
@@ -52,6 +73,13 @@ def main(log_level: str, db_dir: str, url: str) -> None:
     with VideoCaptureOpener(url) as video_capture:
         video_info = get_video_info(video_capture)
         logging.debug("video_info = %s", video_info)
+        selected_frame_indexes = select_frames(
+            n_frames=video_info["n_frames"],
+            fps=video_info["fps"],
+            max_frames_per_second=1.0,
+            max_frames_per_video=300,
+        )
+        logging.debug("selected_frame_indexes = %s", selected_frame_indexes)
 
 
 if __name__ == "__main__":
