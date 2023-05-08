2f288758853106cf44c8d050c95da0b04b25324a
kleamp1e <kleamp1e@gmail.com>
2023-05-08 23:10:07 +0900

    リファクタリング

--- ocaz-sandbox/src/ocaz_sandbox/add_url.py
+++ ocaz-sandbox/src/ocaz_sandbox/add_url.py
@@ -11,10 +11,6 @@ import pymongo
 COLLECTION_URL = "url"
 
 
-def make_url_id(url):
-    return hashlib.sha1(url.encode("utf-8")).hexdigest()
-
-
 def read_urls_from_stdin():
     urls = []
     while url := sys.stdin.readline():
@@ -22,7 +18,11 @@ def read_urls_from_stdin():
     return urls
 
 
-def add_urls_into_mongodb(mongo_db, urls):
+def make_url_id(url):
+    return hashlib.sha1(url.encode("utf-8")).hexdigest()
+
+
+def bulk_upsert_urls(mongodb, urls):
     logging.info(f"urls.length = {len(urls)}")
     operations = [
         pymongo.UpdateOne(
@@ -36,39 +36,53 @@ def add_urls_into_mongodb(mongo_db, urls):
         )
         for url in urls
     ]
-    mongo_db[COLLECTION_URL].bulk_write(operations)
+    mongodb[COLLECTION_URL].bulk_write(operations)
 
 
-@click.command()
-@click.option(
-    "--mongodb-url",
-    type=str,
-    required=True,
-    default=os.environ.get("OCAZ_MONGODB_URL", None),
-)
-@click.option("--stdin/--no-stdin", type=bool, default=False)
-@click.argument("urls", type=str, nargs=-1)
-def main(mongodb_url, stdin, urls):
+def add_url(mongodb_url: str, stdin: bool, urls: list[str]):
     logging.debug(f"mongodb_url = {json.dumps(mongodb_url)}")
+    logging.debug(f"stdin = {json.dumps(stdin)}")
+    logging.debug(f"urls.length = {len(urls)}")
 
     target_urls = []
     target_urls.extend(list(urls))
     if stdin:
         target_urls.extend(read_urls_from_stdin())
-
     logging.info(f"target_urls.length = {len(target_urls)}")
 
-    mongo_db = pymongo.MongoClient(mongodb_url).get_database()
+    mongodb = pymongo.MongoClient(mongodb_url).get_database()
 
     for urls in more_itertools.chunked(target_urls, 1000):
-        add_urls_into_mongodb(mongo_db, urls)
+        bulk_upsert_urls(mongodb, urls)
 
+
+@click.command()
+@click.option(
+    "-l",
+    "--log-level",
+    type=click.Choice(["info", "debug"]),
+    default="info",
+    show_default=True,
+    help="log level",
+)
+@click.option(
+    "--mongodb-url",
+    type=str,
+    required=True,
+    default=os.environ.get("OCAZ_MONGODB_URL", None),
+    show_default=True,
+)
+@click.option("--stdin/--no-stdin", type=bool, default=False)
+@click.argument("urls", type=str, nargs=-1)
+def main(log_level: str, mongodb_url: str, stdin: bool, urls: list[str]):
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)s %(message)s",
+        level=getattr(logging, log_level.upper(), logging.INFO),
+    )
+    logging.debug(f"log_level = {json.dumps(log_level)}")
+    add_url(mongodb_url=mongodb_url, stdin=stdin, urls=urls)
     logging.info("done")
 
 
 if __name__ == "__main__":
-    logging.basicConfig(
-        format="%(asctime)s %(levelname)s %(message)s",
-        level=logging.INFO,
-    )
     main()
