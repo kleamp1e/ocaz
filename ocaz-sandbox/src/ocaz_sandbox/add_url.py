8e0adb561165844ba6107c6d39916716ca423f78
kleamp1e <kleamp1e@gmail.com>
2023-05-10 01:17:49 +0900

    チャンクサイズをコマンドライン引数で指定できるように変更

--- ocaz-sandbox/src/ocaz_sandbox/add_url.py
+++ ocaz-sandbox/src/ocaz_sandbox/add_url.py
@@ -44,9 +44,12 @@ def bulk_upsert_urls(mongodb: pymongo.database.Database, urls: List[str]) -> Non
     mongodb[COLLECTION_URL].bulk_write(operations)
 
 
-def add_url(mongodb: pymongo.database.Database, urls: List[str], chunk_size: int = 1000) -> None:
-    logging.debug(f"urls.length = {len(urls)}")
+def add_url(mongodb_url: str, chunk_size: int, urls: List[str]) -> None:
+    logging.debug(f"mongodb_url = {json.dumps(mongodb_url)}")
     logging.debug(f"chunk_size = {chunk_size}")
+    logging.debug(f"urls.length = {len(urls)}")
+
+    mongodb = get_database(mongodb_url)
 
     for chunked_urls in more_itertools.chunked(urls, chunk_size):
         bulk_upsert_urls(mongodb, chunked_urls)
@@ -56,23 +59,21 @@ def add_url(mongodb: pymongo.database.Database, urls: List[str], chunk_size: int
 @option_log_level
 @option_mongodb_url
 @click.option("--stdin/--no-stdin", type=bool, default=False)
+@click.option("--chunk-size", type=int, required=True, default=1000, show_default=True)
 @click.argument("urls", type=str, nargs=-1)
-def main(log_level: str, mongodb_url: str, stdin: bool, urls: List[str]) -> None:
+def main(log_level: str, mongodb_url: str, stdin: bool, chunk_size: int, urls: List[str]) -> None:
     logging.basicConfig(
         format="%(asctime)s %(levelname)s %(message)s",
         level=getattr(logging, log_level.upper(), logging.INFO),
     )
     logging.debug(f"log_level = {json.dumps(log_level)}")
-    logging.debug(f"mongodb_url = {json.dumps(mongodb_url)}")
     logging.debug(f"stdin = {json.dumps(stdin)}")
-
-    mongodb = get_database(mongodb_url)
 
     urls = list(urls)
     if stdin:
         urls.extend(read_urls_from_stdin())
 
-    add_url(mongodb, urls)
+    add_url(mongodb_url=mongodb_url, chunk_size=chunk_size, urls=urls)
 
     logging.info("done")
 
