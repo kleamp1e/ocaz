67e67c323f240d6475e2a2198278a3c238a330d9
kleamp1e <kleamp1e@gmail.com>
2023-05-15 01:17:21 +0900

    オブジェクトのURLを取得する処理を追加

--- ocaz-sandbox/src/ocaz_sandbox/video_digester.py
+++ ocaz-sandbox/src/ocaz_sandbox/video_digester.py
@@ -1,6 +1,40 @@
+import json
+import logging
+import os
+import re
+from typing import Any, Optional
 
-from typing import Any
 import fastapi
+import pymongo
+from fastapi.responses import RedirectResponse
+
+from .db import COLLECTION_OBJECT, COLLECTION_URL, get_database
+
+
+def not_found() -> fastapi.HTTPException:
+    return fastapi.HTTPException(status_code=404, detail="not found")
+
+
+SHA1_PATTERN = re.compile(r"^[0-9a-f]{40}$")
+
+
+def is_sha1(sha1: str) -> bool:
+    return SHA1_PATTERN.match(sha1) is not None
+
+
+def get_url_from_head_10mb_sha1(mongodb: pymongo.database.Database, head_10mb_sha1: str) -> Optional[str]:
+    if record := mongodb[COLLECTION_URL].find_one({"head10mbSha1": head_10mb_sha1, "available": True}, {"url": True}):
+        return record["url"]
+    else:
+        return None
+
+
+OCAZ_MONGODB_URL = os.environ["OCAZ_MONGODB_URL"]
+CACHE_DIR = os.environ["CACHE_DIR"]
+logging.debug(f"OCAZ_MONGODB_URL = {json.dumps(OCAZ_MONGODB_URL)}")
+logging.debug(f"CACHE_DIR = {json.dumps(CACHE_DIR)}")
+
+mongodb = get_database(OCAZ_MONGODB_URL)
 
 app = fastapi.FastAPI()
 
@@ -8,3 +42,11 @@ app = fastapi.FastAPI()
 @app.get("/")
 def get_root() -> Any:
     return {}
+
+
+@app.get("/object/head10mbSha1/{head_10mb_sha1}")
+def get_object_head_10mb_sha1(head_10mb_sha1: str) -> Any:
+    if is_sha1(head_10mb_sha1) and (url := get_url_from_head_10mb_sha1(mongodb, head_10mb_sha1)):
+        return RedirectResponse(url)
+    else:
+        raise not_found()
