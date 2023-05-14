ed39a595622bb73fef41fd7c90faab619604b12c
kleamp1e <kleamp1e@gmail.com>
2023-05-15 01:35:27 +0900

    ダイジェスト動画を生成する処理を追加

--- ocaz-sandbox/src/ocaz_sandbox/video_digester.py
+++ ocaz-sandbox/src/ocaz_sandbox/video_digester.py
@@ -1,11 +1,15 @@
+import contextlib
 import hashlib
 import json
+import math
 import os
 import pathlib
 import re
-from typing import Any, Optional
+from typing import Any, Dict, Optional
 
+import cv2
 import fastapi
+import numpy as np
 import pymongo
 from fastapi.responses import RedirectResponse
 
@@ -28,6 +32,61 @@ def get_url_from_head_10mb_sha1(mongodb: pymongo.database.Database, head_10mb_sh
         return record["url"]
     else:
         return None
+
+
+@contextlib.contextmanager
+def open_video_capture(url: str) -> cv2.VideoCapture:
+    video_capture = cv2.VideoCapture(url)
+    assert video_capture.isOpened()
+    try:
+        yield video_capture
+    finally:
+        video_capture.release()
+
+
+def get_video_properties(video_capture: cv2.VideoCapture) -> Dict[str, Any]:
+    return {
+        "width": int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH)),
+        "height": int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT)),
+        "numberOfFrames": int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT)),
+        "fps": video_capture.get(cv2.CAP_PROP_FPS),
+    }
+
+
+def read_frame(video_capture: cv2.VideoCapture, frame_index: int = None) -> np.ndarray:
+    if frame_index:
+        assert video_capture.set(cv2.CAP_PROP_POS_FRAMES, frame_index)
+    ret, frame = video_capture.read()
+    assert ret
+    return frame
+
+
+def extract_key_frame_indexes(number_of_frames, n_blocks):
+    interval = number_of_frames / (n_blocks + 1)
+    return [math.floor(interval * (i + 1)) for i in range(n_blocks)]
+
+
+def expand_frame_indexes(key_frame_indexes, frames_per_block, number_of_frames):
+    frame_indexes = set()
+
+    for key_frame_index in key_frame_indexes:
+        for n in range(frames_per_block):
+            frame_index = math.floor(key_frame_index - frames_per_block / 2 + n)
+            if 0 <= frame_index <= number_of_frames - 1:
+                frame_indexes.add(frame_index)
+
+    return sorted(list(frame_indexes))
+
+
+def calc_output_size(width, height, max_size):
+    if width > height:
+        return (max_size, math.floor((height / width) * max_size))
+    else:
+        return (math.floor((width / height) * max_size), max_size)
+
+
+def make_nested_id_name(id: str, ext: str = "") -> str:
+    return f"{id[0:2]}/{id[2:4]}/{id}{ext}"
 
 
 OCAZ_MONGODB_URL = os.environ["OCAZ_MONGODB_URL"]
@@ -62,6 +121,43 @@ def get_object_head_10mb_sha1(head_10mb_sha1: str, number_of_blocks: int = 10, m
             file.write(config_json)
 
     if is_sha1(head_10mb_sha1) and (url := get_url_from_head_10mb_sha1(mongodb, head_10mb_sha1)):
+        video_path = CACHE_DIR / config_key / make_nested_id_name(head_10mb_sha1, ".mp4")
+        print(video_path)
+        video_path.parent.mkdir(parents=True, exist_ok=True)
+
+        with open_video_capture(url) as video_capture:
+            video_properties = get_video_properties(video_capture)
+            print(video_properties)
+
+            output_width, output_height = calc_output_size(
+                video_properties["width"], video_properties["height"], max_size
+            )
+
+            output_video = cv2.VideoWriter(
+                str(video_path),
+                cv2.VideoWriter_fourcc(*"mp4v"),
+                video_properties["fps"],
+                (output_width, output_height),
+            )
+
+            key_frame_indexes = extract_key_frame_indexes(
+                number_of_frames=video_properties["numberOfFrames"], n_blocks=number_of_blocks
+            )
+
+            frame_indexes = expand_frame_indexes(
+                key_frame_indexes=key_frame_indexes,
+                frames_per_block=math.floor(video_properties["fps"]),
+                number_of_frames=video_properties["numberOfFrames"],
+            )
+
+            for frame_index in frame_indexes:
+                print(frame_index)
+                frame = read_frame(video_capture, frame_index)
+                resized_frame = cv2.resize(frame, (output_width, output_height))
+                output_video.write(resized_frame)
+
+            output_video.release()
+
         return RedirectResponse(url)
     else:
         raise not_found()
