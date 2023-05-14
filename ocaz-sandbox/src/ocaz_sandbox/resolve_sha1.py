e21e132a1dd96bd72a799ab2907cb948573facb7
kleamp1e <kleamp1e@gmail.com>
2023-05-14 15:57:41 +0900

    sha1を持たないオブジェクトレコードを取得する処理を追加

--- ocaz-sandbox/src/ocaz_sandbox/resolve_sha1.py
+++ ocaz-sandbox/src/ocaz_sandbox/resolve_sha1.py
@@ -1,11 +1,45 @@
 import json
 import logging
+import random
 from typing import Any, Dict, List, Optional
 
 import click
+import pymongo
 
 from .command import option_log_level, option_mongodb_url
 from .db import get_database
+
+COLLECTION_URL = "url"
+COLLECTION_OBJECT = "object"
+
+
+def find_unresolved_objects(mongodb: pymongo.database.Database, max_records: Optional[int] = None) -> Any:
+    records = (
+        mongodb[COLLECTION_OBJECT].find({"sha1": {"$exists": False}}, {"_id": True}).sort("_id", pymongo.ASCENDING)
+    )
+    if max_records:
+        records = records.limit(max_records)
+    return records
+
+
+# def find_unresolved_urls(mongodb: pymongo.database.Database, max_records: Optional[int] = None) -> Any:
+#     records = (
+#         mongodb[COLLECTION_URL]
+#         .find({"head10mbSha1": {"$exists": False}}, {"_id": True, "url": True})
+#         .sort("_id", pymongo.ASCENDING)
+#     )
+#     if max_records:
+#         records = records.limit(max_records)
+#     return records
+
+
+def resolve_sha1(mongodb_url: str, max_records: Optional[int], max_workers: int, chunk_size: int = 100) -> None:
+    mongodb = get_database(mongodb_url)
+
+    object_records = list(find_unresolved_objects(mongodb, max_records))
+    random.shuffle(object_records)
+    logging.info(f"object_records.length = {len(object_records)}")
+    print(object_records)
 
 
 @click.command()
@@ -22,6 +56,8 @@ def main(log_level: str, mongodb_url: str, max_records: Optional[int], max_worke
     logging.debug(f"mongodb_url = {json.dumps(mongodb_url)}")
     logging.debug(f"max_records = {json.dumps(max_records)}")
     logging.debug(f"max_workers = {json.dumps(max_workers)}")
+
+    resolve_sha1(mongodb_url=mongodb_url, max_records=max_records, max_workers=max_workers)
 
     logging.info("done")
 
