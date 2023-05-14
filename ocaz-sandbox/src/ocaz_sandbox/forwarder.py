e5006dbb74761c500740d0acef72bdd8998ce243
kleamp1e <kleamp1e@gmail.com>
2023-05-14 18:11:16 +0900

    リファクタリング

--- ocaz-sandbox/src/ocaz_sandbox/forwarder.py
+++ ocaz-sandbox/src/ocaz_sandbox/forwarder.py
@@ -23,22 +23,22 @@ def is_sha1(sha1):
     return SHA1_PATTERN.match(sha1) is not None
 
 
-def url_sha1_to_url(sha1: str) -> str:
-    if record := mongo_col_url.find_one({"_id": sha1}, {"url": True}):
+def get_url_from_url_sha1(url_sha1: str) -> str:
+    if record := mongo_col_url.find_one({"_id": url_sha1}, {"url": True}):
         return record["url"]
     else:
         return None
 
 
-def object_sha1_to_head_10mb_sha1(sha1: str) -> str:
-    if record := mongo_col_object.find_one({"sha1": sha1}, {"_id": True}):
+def get_head_10mb_sha1_from_sha1(object_id: str) -> str:
+    if record := mongo_col_object.find_one({"sha1": object_id}, {"_id": True}):
         return record["_id"]
     else:
         return None
 
 
-def head_10mb_sha1_to_url(sha1: str) -> str:
-    if record := mongo_col_url.find_one({"head10mbSha1": sha1, "available": True}, {"url": True}):
+def get_url_from_head_10mb_sha1(head_10mb_sha1: str) -> str:
+    if record := mongo_col_url.find_one({"head10mbSha1": head_10mb_sha1, "available": True}, {"url": True}):
         return record["url"]
     else:
         return None
@@ -51,7 +51,7 @@ def get_root():
 
 @app.get("/url/sha1/{url_sha1}")
 def get_url_sha1(url_sha1: str):
-    if is_sha1(url_sha1) and (url := url_sha1_to_url(url_sha1)):
+    if is_sha1(url_sha1) and (url := get_url_from_url_sha1(url_sha1)):
         return RedirectResponse(url)
     else:
         raise not_found()
@@ -59,7 +59,7 @@ def get_url_sha1(url_sha1: str):
 
 @app.get("/object/head10mbSha1/{head_10mb_sha1}")
 def get_object_head_10mb_sha1(head_10mb_sha1: str):
-    if is_sha1(head_10mb_sha1) and (url := head_10mb_sha1_to_url(head_10mb_sha1)):
+    if is_sha1(head_10mb_sha1) and (url := get_url_from_head_10mb_sha1(head_10mb_sha1)):
         return RedirectResponse(url)
     else:
         raise not_found()
@@ -69,8 +69,8 @@ def get_object_head_10mb_sha1(head_10mb_sha1: str):
 def get_object_sha1(object_sha1: str):
     if (
         is_sha1(object_sha1)
-        and (head_10mb_sha1 := object_sha1_to_head_10mb_sha1(object_sha1))
-        and (url := head_10mb_sha1_to_url(head_10mb_sha1))
+        and (head_10mb_sha1 := get_head_10mb_sha1_from_sha1(object_sha1))
+        and (url := get_url_from_head_10mb_sha1(head_10mb_sha1))
     ):
         return RedirectResponse(url)
     else:
