213aa31a6aa4f9dee97a03887013b4e5d4f0c502
kleamp1e <kleamp1e@gmail.com>
2023-05-19 01:14:03 +0900

    MongoDBにアクセスする処理を追加

--- ocaz-simple-browser/app/src/app/api/objects/route.js
+++ ocaz-simple-browser/app/src/app/api/objects/route.js
@@ -1,5 +1,9 @@
+import { MongoClient } from "mongodb";
 import { NextResponse } from "next/server";
 
+const database = new MongoClient(process.env.OCAZ_MONGODB_URL).db();
+
 export async function GET() {
+  console.log(await database.collection("object").find({}).limit(1).toArray());
   return NextResponse.json({});
 }
