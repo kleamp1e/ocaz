07cc950e5198491655a02689b6da4f408861fcb9
kleamp1e <kleamp1e@gmail.com>
2023-05-19 01:32:58 +0900

    オブジェクトの一覧の取得処理を追加

--- ocaz-simple-browser/app/src/app/page.js
+++ ocaz-simple-browser/app/src/app/page.js
@@ -1,5 +1,21 @@
+"use client";
+
 import styles from "./page.module.css";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [objects, setObjects] = useState([]);
+
+  useEffect(() => {
+    console.log("useEffect");
+    async function fetchObjects() {
+      const { objects } = await (await fetch("/api/objects")).json();
+      setObjects(objects);
+    }
+    fetchObjects();
+  }, []);
+
+  console.log({ objects });
+
   return <main className={styles.main}>Root</main>;
 }
