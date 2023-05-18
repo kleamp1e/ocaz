b6857c8224a2c96b837e9c42612f44209d5ec347
kleamp1e <kleamp1e@gmail.com>
2023-05-19 01:27:32 +0900

    forwarderサービスへの転送処理を追加

--- ocaz-simple-browser/app/next.config.js
+++ ocaz-simple-browser/app/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/forwarder/object/head10mbSha1/:head_10mb_sha1",
+        destination:
+          "http://forwarder:8000/object/head10mbSha1/:head_10mb_sha1",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
