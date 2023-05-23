02e5456652696391d441de88a4aa3daa423800ef
kleamp1e <kleamp1e@gmail.com>
2023-05-23 22:00:21 +0900

    スタイル指定を外部CSS化

--- ocaz-simple-browser/app/src/app/components/ImageThumbnail.js
+++ ocaz-simple-browser/app/src/app/components/ImageThumbnail.js
@@ -1,25 +1,18 @@
+import styles from "./ImageThumbnail.module.css";
+
 export default function ImageThumbnail({ object, height }) {
   const width = Math.floor(height * (object.image.width / object.image.height));
   return (
-    <div style={{ border: "2px solid #333333" }}>
+    <div className={styles.outer}>
       <div
+        className={styles.inner}
         style={{
-          position: "relative",
           width: `${width}px`,
           height: `${height}px`,
         }}
       >
         <img
-          style={{
-            position: "absolute",
-            top: "0",
-            bottom: "0",
-            left: "0",
-            right: "0",
-            width: "auto",
-            height: "100%",
-            objectFit: "contain",
-          }}
+          className={styles.image}
           src={`/api/forwarder/object/head10mbSha1/${object.head10mbSha1}`}
           loading="lazy"
         />
