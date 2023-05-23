736c4590d39e2db77c2dc16ce6ecad4d121386b9
kleamp1e <kleamp1e@gmail.com>
2023-05-24 02:02:08 +0900

    ページングのインターフェースを改善

--- ocaz-simple-browser/app/src/app/page.js
+++ ocaz-simple-browser/app/src/app/page.js
@@ -34,14 +34,71 @@ const modalStyle = {
   content: { backgroundColor: "rgba(0, 0, 0, 0.5)" },
 };
 
-function Pagination({ page, setPage, numberOfPages }) {
+function PageLink({ isActive, page, setPage, children }) {
+  const onClick = (e) => {
+    e.preventDefault();
+    setPage(page);
+  };
   return (
-    <div>
-      {Array.from({ length: numberOfPages }, (_, i) => i + 1).map((page) => (
-        <button key={page} onClick={() => setPage(page)}>
-          {page}
-        </button>
-      ))}
+    <span style={{ margin: "0em 0.1em" }}>
+      {isActive ? (
+        <a style={{ cursor: "pointer" }} onClick={onClick}>
+          {children}
+        </a>
+      ) : (
+        children
+      )}
+    </span>
+  );
+}
+
+function Pagination({ page, setPage, numberOfPages }) {
+  const pageSet = new Set([
+    1,
+    2,
+    3,
+    page - 2,
+    page - 1,
+    page,
+    page + 1,
+    page + 2,
+    numberOfPages - 2,
+    numberOfPages - 1,
+    numberOfPages,
+  ]);
+  const pageArray = Array.from(pageSet)
+    .sort((a, b) => a - b)
+    .filter((p) => p >= 1)
+    .filter((p) => p <= numberOfPages)
+    .reduce((memo, p) => {
+      if (memo.length > 0 && memo[memo.length - 1] != p - 1) {
+        memo.push(null);
+      }
+      memo.push(p);
+      return memo;
+    }, []);
+
+  return (
+    <div style={{ fontSize: "26px" }}>
+      <PageLink isActive={page > 1} page={page - 1} setPage={setPage}>
+        {"<<<"}
+      </PageLink>
+      {pageArray.map((p) =>
+        p == null ? (
+          "..."
+        ) : (
+          <PageLink key={p} isActive={p != page} page={p} setPage={setPage}>
+            {p}
+          </PageLink>
+        )
+      )}
+      <PageLink
+        isActive={page < numberOfPages}
+        page={page + 1}
+        setPage={setPage}
+      >
+        {">>>"}
+      </PageLink>{" "}
     </div>
   );
 }
@@ -165,6 +222,11 @@ export default function Page() {
               isOpen: true,
             }))
           }
+        />
+        <Pagination
+          page={context.page}
+          numberOfPages={numberOfPages}
+          setPage={(page) => setContext((prev) => ({ ...prev, page }))}
         />
         <Modal
           isOpen={context.isOpen && context.selectedObjectIndex != null}
