e4efd974e1c6d041d358d024dcf6c35f313db7b9
kleamp1e <kleamp1e@gmail.com>
2023-05-23 22:25:27 +0900

    モーダルウィンドウを表示する処理を追加

--- ocaz-simple-browser/app/src/app/page.js
+++ ocaz-simple-browser/app/src/app/page.js
@@ -1,22 +1,34 @@
 "use client";
 
+import Modal from "react-modal";
 import { useState, useEffect } from "react";
 
 import Thumbnail from "./components/Thumbnail";
 import ThumbnailContainer from "./components/ThumbnailContainer";
 
-function Gallery({ objects, height }) {
+function Gallery({ objects, height, onClick = () => {} }) {
   return (
     <ThumbnailContainer>
       {objects.map((object) => (
-        <Thumbnail key={object.head10mbSha1} object={object} height={height} />
+        <Thumbnail
+          key={object.head10mbSha1}
+          object={object}
+          height={height}
+          onClick={() => onClick(object)}
+        />
       ))}
     </ThumbnailContainer>
   );
 }
 
+const modalStyle = {
+  overlay: { backgroundColor: "rgba(0, 0, 0, 0.7)" },
+  content: { backgroundColor: "#000000" },
+};
+
 export default function Page() {
   const [objects, setObjects] = useState([]);
+  const [selectedObject, setSelectedObject] = useState(null);
 
   useEffect(() => {
     async function fetchObjects() {
@@ -28,7 +40,19 @@ export default function Page() {
 
   return (
     <main>
-      <Gallery objects={objects} height={200} />
+      <Gallery
+        objects={objects}
+        height={200}
+        onClick={(object) => setSelectedObject(object)}
+      />
+      <Modal
+        isOpen={selectedObject != null}
+        onRequestClose={() => setSelectedObject(null)}
+        style={modalStyle}
+      >
+        <div>modal</div>
+        <button onClick={() => setSelectedObject(null)}>close</button>
+      </Modal>
     </main>
   );
 }
