fba6eb2fb1d456d6228b77faa39b7e088b9b2f7c
kleamp1e <kleamp1e@gmail.com>
2023-05-25 23:08:48 +0900

    リファクタリング

--- ocaz-simple-browser/app/src/app/test/page.js
+++ ocaz-simple-browser/app/src/app/test/page.js
@@ -1,21 +1,50 @@
 "use client";
 
-import useSWR from 'swr';
+import useSWR from "swr";
 
-async function find({ }) {
+function deleteNull(params) {
+  const copied = { ...params };
+  for (const key in copied) {
+    if (copied.hasOwnProperty(key) && copied[key] === null) {
+      delete copied[key];
+    }
+  }
+  return copied;
+}
+
+async function find({
+  collection,
+  condition,
+  projection = null,
+  sort = null,
+  limit = null,
+  skip = null,
+}) {
   const params = {
-    collection: "object",
-    condition: JSON.stringify({}),
-    projection: JSON.stringify({"_id":1}),
-    limit: 10,
-    sort: JSON.stringify([["_id", 1]]),
+    collection,
+    condition: condition && JSON.stringify(condition),
+    projection: projection && JSON.stringify(projection),
+    sort: sort && JSON.stringify(sort),
+    limit: limit && String(limit),
+    skip: skip && String(skip),
   };
-  const queryString = new URLSearchParams(params).toString();
-  return fetch(`/api/finder/find?${queryString}`).then((response) => response.json());
+  const queryString = new URLSearchParams(deleteNull(params)).toString();
+  return fetch(`/api/finder/find?${queryString}`).then((response) =>
+    response.json()
+  );
+}
+
+async function findObject(condition) {
+  return find({
+    collection: "object",
+    condition: condition,
+    limit: 10,
+    sort: [["_id", 1]],
+  });
 }
 
 export default function Page() {
-  const { data, error } = useSWR("cond", find)
-  console.log({data});
-  return (<main>test</main>);
+  const { data, error } = useSWR({ mimeType: "image/png" }, findObject);
+  console.log({ data, error });
+  return <main>test</main>;
 }
