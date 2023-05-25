d0d93feb9f8f67ac546b0aeed47a76348a3525ad
kleamp1e <kleamp1e@gmail.com>
2023-05-25 23:28:14 +0900

    ページに表示されるオブジェクトのみレコード全体を取得するように変更

--- ocaz-simple-browser/app/src/app/test/page.js
+++ ocaz-simple-browser/app/src/app/test/page.js
@@ -1,6 +1,9 @@
 "use client";
 
 import useSWR from "swr";
+import { useState } from "react";
+
+import { Pagination, PaginationContent } from "../components/Pagination";
 
 function deleteNull(params) {
   const copied = { ...params };
@@ -34,17 +37,68 @@ async function find({
   );
 }
 
-async function findObject(condition) {
-  return find({
-    collection: "object",
-    condition: condition,
-    limit: 10,
-    sort: [["_id", 1]],
-  });
+function Objects({ objectIds }) {
+  const { data, error } = useSWR(
+    {
+      collection: "object",
+      condition: { _id: { $in: objectIds } },
+    },
+    find
+  );
+  console.log({ data, error });
+
+  if (error) return <div>Error</div>;
+  if (!data) return <div>Loading...</div>;
+
+  return (
+    <ul>
+      {data.records.map((object) => (
+        <li key={object["_id"]}>{JSON.stringify(object)}</li>
+      ))}
+    </ul>
+  );
 }
 
 export default function Page() {
-  const { data, error } = useSWR({ mimeType: "image/png" }, findObject);
+  const [context, setContext] = useState({
+    perPage: 10,
+    page: 1,
+    isOpen: false,
+  });
+  const { data, error } = useSWR(
+    {
+      collection: "object",
+      condition: {},
+      // condition: { mimeType: "image/jpeg" },
+      projection: { _id: 1 },
+      limit: 1000, // DEBUG:
+      sort: [["_id", 1]],
+    },
+    find
+  );
   console.log({ data, error });
-  return <main>test</main>;
+
+  if (error) return <div>Error</div>;
+  if (!data) return <div>Loading...</div>;
+
+  const numberOfPages = Math.ceil(data.records.length / context.perPage);
+  const startIndex = context.perPage * (context.page - 1);
+  const endIndex = context.perPage * context.page;
+  const objectIds = data.records
+    .slice(startIndex, endIndex)
+    .map((object) => object["_id"]);
+  console.log({ numberOfPages, context });
+
+  return (
+    <main>
+      <Pagination
+        page={context.page}
+        numberOfPages={numberOfPages}
+        setPage={(page) => setContext((prev) => ({ ...prev, page }))}
+      />
+      <PaginationContent>
+        <Objects objectIds={objectIds} />
+      </PaginationContent>
+    </main>
+  );
 }
