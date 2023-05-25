7765452f41bb0d37e671291e7a9d1bab2cd6d8e3
kleamp1e <kleamp1e@gmail.com>
2023-05-25 22:12:08 +0900

    検索処理を追加

--- ocaz-finder/src/ocaz_finder/app.py
+++ ocaz-finder/src/ocaz_finder/app.py
@@ -1,10 +1,58 @@
-from typing import Any
+import json
+import os
+from typing import Any, Optional
 
 import fastapi
+import pymongo
 
+
+def get_database(mongodb_url: str) -> pymongo.database.Database:
+    return pymongo.MongoClient(mongodb_url).get_database()
+
+
+mongodb = get_database(os.environ["OCAZ_MONGODB_URL"])
 app = fastapi.FastAPI()
 
 
 @app.get("/")
 def get_root() -> Any:
     return {}
+
+
+@app.get("/query")
+def get_query() -> Any:
+    return {
+        "queries": [
+            {"name": "image", "object": {"condition": {"mimeType": {"$in": ["image/jpeg", "image/png", "image/gif"]}}}}
+        ]
+    }
+
+
+@app.get("/find")
+def get_find(
+    condition: str,
+    projection: Optional[str] = None,
+    sort: Optional[str] = None,
+    limit: Optional[int] = None,
+    skip: Optional[int] = None,
+) -> Any:
+    condition = json.loads(condition)
+    projection = json.loads(projection) if projection is not None else None
+
+    records = mongodb["object"].find(condition, projection)
+
+    if sort is not None:
+        records = records.sort(json.loads(sort))
+    if limit is not None:
+        records = records.limit(limit)
+    if skip is not None:
+        records = records.skip(skip)
+
+    return {
+        "condition": condition,
+        "projection": projection,
+        "sort": sort,
+        "limit": limit,
+        "skip": skip,
+        "result": list(records),
+    }
