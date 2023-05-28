b1ad535d25f59c8e265304cbba9bf62a60a97b01
kleamp1e <kleamp1e@gmail.com>
2023-05-28 18:45:48 +0900

    まだ推論が終わっていないオブジェクトIDを取得する処理を追加

--- ocaz-sandbox/src/ocaz_sandbox/predict_nsfw_opennsfw2.py
+++ ocaz-sandbox/src/ocaz_sandbox/predict_nsfw_opennsfw2.py
@@ -2,7 +2,7 @@ import json
 import logging
 import os
 from datetime import datetime
-from typing import Any, Dict, Optional
+from typing import Any, Dict, List, Optional
 
 import click
 import pymongo
@@ -13,7 +13,20 @@ from .db import get_database
 
 COLLECTION_URL = "url"
 COLLECTION_OBJECT = "object"
+OCAZ_CLASSIFIER_NSFW_OPENNSFW2_NAME = "ocaz-classifier-nsfw-opennsfw2"
 OCAZ_CLASSIFIER_NSFW_OPENNSFW2_BASE_URL = "http://ocaz-classifier-nsfw-opennsfw2:8000"
+TARGET_FIELD = f"image.predictions.{OCAZ_CLASSIFIER_NSFW_OPENNSFW2_NAME}"
+
+
+def find_unpredicted_object_ids(mongodb: pymongo.database.Database, max_records: Optional[int] = None) -> List[str]:
+    records = (
+        mongodb[COLLECTION_OBJECT]
+        .find({"image": {"$exists": True}, TARGET_FIELD: {"$exists": False}})
+        .sort("_id", pymongo.ASCENDING)
+    )
+    if max_records:
+        records = records.limit(max_records)
+    return [record["_id"] for record in records]
 
 
 def find_object(mongodb: pymongo.database.Database, object_id: str) -> Any:
@@ -36,15 +49,21 @@ def predict(base_url: str, bin: bytes, file_name: str, mime_type: str) -> Dict:
     return response.json()
 
 
-def predict_nsfw_opennsfw2(mongodb_url: str, classifier_nsfw_opennsfw2_base_url: str) -> None:
+def predict_nsfw_opennsfw2(
+    mongodb_url: str, classifier_nsfw_opennsfw2_base_url: str, max_records: Optional[int]
+) -> None:
     logging.debug(f"mongodb_url = {json.dumps(mongodb_url)}")
     logging.debug(f"classifier_nsfw_opennsfw2_base_url = {json.dumps(classifier_nsfw_opennsfw2_base_url)}")
-    # logging.debug(f"max_records = {json.dumps(max_records)}")
+    logging.debug(f"max_records = {json.dumps(max_records)}")
     # logging.debug(f"max_workers = {json.dumps(max_workers)}")
     # logging.debug(f"chunk_size = {json.dumps(chunk_size)}")
 
     mongodb = get_database(mongodb_url)
 
+    object_ids = find_unpredicted_object_ids(mongodb=mongodb, max_records=max_records)
+    print(object_ids)
+
+    """
     # TODO: 推論が完了していないobjectレコードを取得する
     object_id = "000025890764a9e19bbb11cd140b4f8d1e69fe44"
     logging.info(f"object_id = {object_id}")
@@ -76,7 +95,7 @@ def predict_nsfw_opennsfw2(mongodb_url: str, classifier_nsfw_opennsfw2_base_url:
             {"_id": object_id},
             {
                 "$set": {
-                    f'image.predictions.{prediction_result["service"]["name"]}': {
+                    TARGET_FIELD: {
                         "version": prediction_result["service"]["version"],
                         "predictedAt": now.timestamp(),
                         "labels": prediction_result["labels"],
@@ -89,15 +108,12 @@ def predict_nsfw_opennsfw2(mongodb_url: str, classifier_nsfw_opennsfw2_base_url:
     print(operations)
     if len(operations) > 0:
         mongodb[COLLECTION_OBJECT].bulk_write(operations)
+    """
 
 
 @click.command()
 @option_log_level
 @option_mongodb_url
-# @click.option("--max-records", type=int, default=None, show_default=True)
-# @click.option("--max-workers", type=int, default=4, show_default=True, required=True)
-# @click.option("--chunk-size", type=int, default=10, show_default=True, required=True)
-# max_records: Optional[int], max_workers: int, chunk_size: int
 @click.option(
     "--classifier-nsfw-opennsfw2-base-url",
     type=str,
@@ -105,10 +121,15 @@ def predict_nsfw_opennsfw2(mongodb_url: str, classifier_nsfw_opennsfw2_base_url:
     show_default=True,
     required=True,
 )
+@click.option("--max-records", type=int, default=None, show_default=True)
+# @click.option("--max-workers", type=int, default=4, show_default=True, required=True)
+# @click.option("--chunk-size", type=int, default=10, show_default=True, required=True)
+# max_workers: int, chunk_size: int
 def main(
     log_level: str,
     mongodb_url: str,
     classifier_nsfw_opennsfw2_base_url: str,
+    max_records: Optional[int],
 ) -> None:
     logging.basicConfig(
         format="%(asctime)s %(levelname)s pid:%(process)d %(message)s",
@@ -117,7 +138,9 @@ def main(
     logging.debug(f"log_level = {json.dumps(log_level)}")
 
     predict_nsfw_opennsfw2(
-        mongodb_url=mongodb_url, classifier_nsfw_opennsfw2_base_url=classifier_nsfw_opennsfw2_base_url
+        mongodb_url=mongodb_url,
+        classifier_nsfw_opennsfw2_base_url=classifier_nsfw_opennsfw2_base_url,
+        max_records=max_records,
     )
 
     logging.info("done")
