637bce6c78aaacaf1741b2ed66e037e3f746cfd0
kleamp1e <kleamp1e@gmail.com>
2023-05-28 18:24:12 +0900

    分類器のURLをコマンドライン引数で指定できるように変更

--- ocaz-sandbox/src/ocaz_sandbox/predict_nsfw_opennsfw2.py
+++ ocaz-sandbox/src/ocaz_sandbox/predict_nsfw_opennsfw2.py
@@ -1,14 +1,9 @@
-import concurrent.futures
-import contextlib
 import json
 import logging
-from typing import Any, List, Optional
+import os
+from typing import Any, Optional
 
 import click
-import cv2
-import more_itertools
-import numpy as np
-import PIL.Image
 import pymongo
 import requests
 
@@ -31,8 +26,9 @@ def find_url(mongodb: pymongo.database.Database, object_id: str) -> Optional[str
         return None
 
 
-def predict_nsfw_opennsfw2(mongodb_url: str) -> None:
+def predict_nsfw_opennsfw2(mongodb_url: str, classifier_nsfw_opennsfw2_base_url: str) -> None:
     logging.debug(f"mongodb_url = {json.dumps(mongodb_url)}")
+    logging.debug(f"classifier_nsfw_opennsfw2_base_url = {json.dumps(classifier_nsfw_opennsfw2_base_url)}")
     # logging.debug(f"max_records = {json.dumps(max_records)}")
     # logging.debug(f"max_workers = {json.dumps(max_workers)}")
     # logging.debug(f"chunk_size = {json.dumps(chunk_size)}")
@@ -55,7 +51,7 @@ def predict_nsfw_opennsfw2(mongodb_url: str) -> None:
     print()
 
     classifier_response = requests.post(
-        OCAZ_CLASSIFIER_NSFW_OPENNSFW2_BASE_URL + "/classify",
+        classifier_nsfw_opennsfw2_base_url + "/classify",
         files={"file": (object_id, object_response.content, object_record["mimeType"])},
     )
     print(classifier_response)
@@ -70,9 +66,17 @@ def predict_nsfw_opennsfw2(mongodb_url: str) -> None:
 # @click.option("--max-workers", type=int, default=4, show_default=True, required=True)
 # @click.option("--chunk-size", type=int, default=10, show_default=True, required=True)
 # max_records: Optional[int], max_workers: int, chunk_size: int
+@click.option(
+    "--classifier-nsfw-opennsfw2-base-url",
+    type=str,
+    default=os.environ.get("OCAZ_CLASSIFIER_NSFW_OPENNSFW2_BASE_URL", None),
+    show_default=True,
+    required=True,
+)
 def main(
     log_level: str,
     mongodb_url: str,
+    classifier_nsfw_opennsfw2_base_url: str,
 ) -> None:
     logging.basicConfig(
         format="%(asctime)s %(levelname)s pid:%(process)d %(message)s",
@@ -80,7 +84,9 @@ def main(
     )
     logging.debug(f"log_level = {json.dumps(log_level)}")
 
-    predict_nsfw_opennsfw2(mongodb_url=mongodb_url)
+    predict_nsfw_opennsfw2(
+        mongodb_url=mongodb_url, classifier_nsfw_opennsfw2_base_url=classifier_nsfw_opennsfw2_base_url
+    )
 
     logging.info("done")
 
