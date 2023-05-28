d999678f31b84956fbd235cce38732a0d0b248d2
kleamp1e <kleamp1e@gmail.com>
2023-05-28 19:00:00 +0900

    並列処理、バッチ処理を追加

--- ocaz-sandbox/src/ocaz_sandbox/predict_nsfw_opennsfw2.py
+++ ocaz-sandbox/src/ocaz_sandbox/predict_nsfw_opennsfw2.py
@@ -1,10 +1,13 @@
+import concurrent.futures
 import json
 import logging
 import os
+import random
 from datetime import datetime
 from typing import Any, Dict, List, Optional
 
 import click
+import more_itertools
 import pymongo
 import requests
 
@@ -29,7 +32,7 @@ def find_unpredicted_object_ids(mongodb: pymongo.database.Database, max_records:
     return [record["_id"] for record in records]
 
 
-def find_object(mongodb: pymongo.database.Database, object_id: str) -> Any:
+def find_object(mongodb: pymongo.database.Database, object_id: str) -> Optional[Any]:
     return mongodb[COLLECTION_OBJECT].find_one({"_id": object_id})
 
 
@@ -38,6 +41,13 @@ def find_url(mongodb: pymongo.database.Database, object_id: str) -> Optional[str
         return record["url"]
     else:
         return None
+
+
+def get(url: str) -> Any:
+    logging.info(f"get {url}")
+    response = requests.get(url)
+    assert response.status_code == requests.codes.ok
+    return response
 
 
 def predict(base_url: str, bin: bytes, file_name: str, mime_type: str) -> Dict:
@@ -49,66 +59,76 @@ def predict(base_url: str, bin: bytes, file_name: str, mime_type: str) -> Dict:
     return response.json()
 
 
+def predict_objects(mongodb_url: str, classifier_nsfw_opennsfw2_base_url: str, object_ids: List[str]) -> None:
+    logging.info(f"object_ids.length = {len(object_ids)}")
+
+    mongodb = get_database(mongodb_url)
+    operations = []
+
+    for object_id in object_ids:
+        logging.info(f"object_id = {object_id}")
+
+        object_record = find_object(mongodb, object_id)
+        object_url = find_url(mongodb, object_id)
+        object_response = get(object_url)
+
+        prediction = predict(
+            base_url=classifier_nsfw_opennsfw2_base_url,
+            bin=object_response.content,
+            file_name=object_id,
+            mime_type=object_record["mimeType"],
+        )
+
+        now = datetime.now()
+        operations.append(
+            pymongo.UpdateOne(
+                {"_id": object_id},
+                {
+                    "$set": {
+                        TARGET_FIELD: {
+                            "version": prediction["service"]["version"],
+                            "predictedAt": now.timestamp(),
+                            "labels": prediction["labels"],
+                        },
+                        "updatedAt": now.timestamp(),
+                    },
+                },
+            )
+        )
+
+    if len(operations) > 0:
+        mongodb[COLLECTION_OBJECT].bulk_write(operations)
+
+
 def predict_nsfw_opennsfw2(
-    mongodb_url: str, classifier_nsfw_opennsfw2_base_url: str, max_records: Optional[int]
+    mongodb_url: str,
+    classifier_nsfw_opennsfw2_base_url: str,
+    max_records: Optional[int],
+    max_workers: int,
+    chunk_size: int,
 ) -> None:
     logging.debug(f"mongodb_url = {json.dumps(mongodb_url)}")
     logging.debug(f"classifier_nsfw_opennsfw2_base_url = {json.dumps(classifier_nsfw_opennsfw2_base_url)}")
     logging.debug(f"max_records = {json.dumps(max_records)}")
-    # logging.debug(f"max_workers = {json.dumps(max_workers)}")
-    # logging.debug(f"chunk_size = {json.dumps(chunk_size)}")
+    logging.debug(f"max_workers = {json.dumps(max_workers)}")
+    logging.debug(f"chunk_size = {json.dumps(chunk_size)}")
 
     mongodb = get_database(mongodb_url)
 
     object_ids = find_unpredicted_object_ids(mongodb=mongodb, max_records=max_records)
-    print(object_ids)
+    random.shuffle(object_ids)
+    logging.info(f"object_ids.length = {len(object_ids)}")
 
-    """
-    # TODO: 推論が完了していないobjectレコードを取得する
-    object_id = "000025890764a9e19bbb11cd140b4f8d1e69fe44"
-    logging.info(f"object_id = {object_id}")
-
-    object_record = find_object(mongodb, object_id)
-    print(object_record)
-
-    object_url = find_url(mongodb, object_id)
-    logging.info(f"object_url = {object_url}")
-
-    object_response = requests.get(object_url)
-    print(object_response)
-    assert object_response.status_code == requests.codes.ok
-    print()
-
-    prediction_result = predict(
-        base_url=classifier_nsfw_opennsfw2_base_url,
-        bin=object_response.content,
-        file_name=object_id,
-        mime_type=object_record["mimeType"],
-    )
-    print(prediction_result)
-
-    now = datetime.now()
-
-    operations = []
-    operations.append(
-        pymongo.UpdateOne(
-            {"_id": object_id},
-            {
-                "$set": {
-                    TARGET_FIELD: {
-                        "version": prediction_result["service"]["version"],
-                        "predictedAt": now.timestamp(),
-                        "labels": prediction_result["labels"],
-                    },
-                    "updatedAt": now.timestamp(),
-                },
-            },
-        )
-    )
-    print(operations)
-    if len(operations) > 0:
-        mongodb[COLLECTION_OBJECT].bulk_write(operations)
-    """
+    with concurrent.futures.ProcessPoolExecutor(max_workers=max_workers) as executor:
+        try:
+            results = [
+                executor.submit(predict_objects, mongodb_url, classifier_nsfw_opennsfw2_base_url, chunked_object_ids)
+                for chunked_object_ids in more_itertools.chunked(object_ids, chunk_size)
+            ]
+            for result in results:
+                result.result()
+        except KeyboardInterrupt:
+            executor.shutdown(wait=False)
 
 
 @click.command()
@@ -122,14 +142,15 @@ def predict_nsfw_opennsfw2(
     required=True,
 )
 @click.option("--max-records", type=int, default=None, show_default=True)
-# @click.option("--max-workers", type=int, default=4, show_default=True, required=True)
-# @click.option("--chunk-size", type=int, default=10, show_default=True, required=True)
-# max_workers: int, chunk_size: int
+@click.option("--max-workers", type=int, default=4, show_default=True, required=True)
+@click.option("--chunk-size", type=int, default=10, show_default=True, required=True)
 def main(
     log_level: str,
     mongodb_url: str,
     classifier_nsfw_opennsfw2_base_url: str,
     max_records: Optional[int],
+    max_workers: int,
+    chunk_size: int,
 ) -> None:
     logging.basicConfig(
         format="%(asctime)s %(levelname)s pid:%(process)d %(message)s",
@@ -141,6 +162,8 @@ def main(
         mongodb_url=mongodb_url,
         classifier_nsfw_opennsfw2_base_url=classifier_nsfw_opennsfw2_base_url,
         max_records=max_records,
+        max_workers=max_workers,
+        chunk_size=chunk_size,
     )
 
     logging.info("done")
