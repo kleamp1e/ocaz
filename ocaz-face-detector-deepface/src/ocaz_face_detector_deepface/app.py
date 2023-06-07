54549a4f7bb6e90c4c01da8e4a10bafa22b5d4e7
kleamp1e <kleamp1e@gmail.com>
2023-06-07 11:33:46 +0900

    リファクタリング

--- ocaz-face-detector-deepface/src/ocaz_face_detector_deepface/app.py
+++ ocaz-face-detector-deepface/src/ocaz_face_detector_deepface/app.py
@@ -6,14 +6,13 @@ import cv2
 import numpy as np
 from deepface import DeepFace
 from deepface.commons import functions
-from deepface.extendedmodels import Gender, Race
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 from .const import service
 from .cv_util import VideoProperties, get_video_properties, open_video_capture, read_frame
-from .face_detector import AgeEstimator, EmotionClassifier
+from .face_detector import AgeEstimator, EmotionClassifier, RaceClassifier, SexClassifier
 
 app = FastAPI()
 app.add_middleware(
@@ -35,23 +34,8 @@ async def get_about() -> Any:
 
 emotion_classifier = EmotionClassifier()
 age_estimator = AgeEstimator()
-models = {}
-models["gender"] = DeepFace.build_model("Gender")
-models["race"] = DeepFace.build_model("Race")
-
-
-def predict_sex(model, image):
-    assert image.shape == (1, 224, 224, 3)
-    predictions = model.predict(image, verbose=0)[0]
-    sum = predictions.sum()
-    return {label: predictions[i] / sum for i, label in enumerate(Gender.labels)}
-
-
-def predict_race(model, image):
-    assert image.shape == (1, 224, 224, 3)
-    predictions = model.predict(image, verbose=0)[0]
-    sum = predictions.sum()
-    return {label: predictions[i] / sum for i, label in enumerate(Race.labels)}
+sex_classifier = SexClassifier()
+race_classifier = RaceClassifier()
 
 
 # @app.get("/detect", response_model=DetectResponse)
@@ -99,8 +83,8 @@ async def get_detect(url: str, frame_indexes: str = "0") -> Any:
             # print(predict_emotion(models["emotion"], img_content))
             print(emotion_classifier.predict(img_content))
             print(age_estimator.predict(img_content))
-            print(predict_sex(models["gender"], img_content))
-            print(predict_race(models["race"], img_content))
+            print(sex_classifier.predict(img_content))
+            print(race_classifier.predict(img_content))
 
     return {
         "service": service,
--- ocaz-face-detector-deepface/src/ocaz_face_detector_deepface/face_detector.py
+++ ocaz-face-detector-deepface/src/ocaz_face_detector_deepface/face_detector.py
@@ -5,6 +5,7 @@ from deepface.commons import functions
 from deepface.extendedmodels import Age, Emotion, Gender, Race
 
 
+# REF: https://github.com/serengil/deepface/blob/ce4e4f664b66c05e682de8c0913798da0420dae1/deepface/DeepFace.py#L230
 class EmotionClassifier:
     def __init__(self):
         self.model = DeepFace.build_model("Emotion")
@@ -22,6 +23,7 @@ class EmotionClassifier:
         return {label: predictions[i] / sum for i, label in enumerate(Emotion.labels)}
 
 
+# REF: https://github.com/serengil/deepface/blob/ce4e4f664b66c05e682de8c0913798da0420dae1/deepface/DeepFace.py#L230
 class AgeEstimator:
     def __init__(self):
         self.model = DeepFace.build_model("Age")
@@ -30,3 +32,27 @@ class AgeEstimator:
         assert images.shape == (1, 224, 224, 3)
         predictions = self.model.predict(images, verbose=0)[0]
         return Age.findApparentAge(predictions)
+
+
+# REF: https://github.com/serengil/deepface/blob/ce4e4f664b66c05e682de8c0913798da0420dae1/deepface/DeepFace.py#L230
+class SexClassifier:
+    def __init__(self):
+        self.model = DeepFace.build_model("Gender")
+
+    def predict(self, images):
+        assert images.shape == (1, 224, 224, 3)
+        predictions = self.model.predict(images, verbose=0)[0]
+        sum = predictions.sum()
+        return {label: predictions[i] / sum for i, label in enumerate(Gender.labels)}
+
+
+# REF: https://github.com/serengil/deepface/blob/ce4e4f664b66c05e682de8c0913798da0420dae1/deepface/DeepFace.py#L230
+class RaceClassifier:
+    def __init__(self):
+        self.model = DeepFace.build_model("Race")
+
+    def predict(self, images):
+        assert images.shape == (1, 224, 224, 3)
+        predictions = self.model.predict(images, verbose=0)[0]
+        sum = predictions.sum()
+        return {label: predictions[i] / sum for i, label in enumerate(Race.labels)}
