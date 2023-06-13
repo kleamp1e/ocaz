3d6cf22570bda548e752ca8b0840a56bbde372b3
kleamp1e <kleamp1e@gmail.com>
2023-06-13 23:21:07 +0900

    フレーム辺りの検出された顔の数を出力する処理を追加

--- ocaz-face-detector-deepface/src/ocaz_face_detector_deepface/app.py
+++ ocaz-face-detector-deepface/src/ocaz_face_detector_deepface/app.py
@@ -1,13 +1,23 @@
 from dataclasses import asdict
 from datetime import datetime
-from typing import Any
+from typing import Any, List, Tuple
 
+import numpy as np
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from .const import service
 from .cv_util import get_video_properties, open_video_capture, read_frame
 from .face_extractor import FaceExtractor
+
+def convert_to_frames_array(frame_faces_pairs: List[Tuple[int, Any]]) -> np.ndarray:
+    return np.array(
+        [(frame_index, len(faces)) for frame_index, faces in frame_faces_pairs],
+        dtype=[
+            ("frameIndex", np.uint32),
+            ("numberOfFaces", np.uint16),
+        ],
+    )
 
 face_extractor = FaceExtractor()
 
@@ -36,21 +46,20 @@ async def get_detect(url: str, frame_indexes: str = "0") -> Any:
 
     with open_video_capture(url) as video_capture:
         video_properties = get_video_properties(video_capture)
-        # frame_faces_pairs = []
+        frame_faces_pairs = []
         for frame_index in frame_indexes:
             frame = read_frame(video_capture, frame_index=frame_index)
-        #     faces = face_detector.detect(frame)
-        #     frame_faces_pairs.append((frame_index, faces))
+            faces = face_extractor.extract(frame)
+            for face in faces:
+                face_dict = asdict(face)
+                del face_dict["alignedImage"]
+                del face_dict["facenet512"]
+                print(face_dict)
+            frame_faces_pairs.append((frame_index, faces))
 
-    # frames_array = convert_to_frames_array(frame_faces_pairs)
+    frames_array = convert_to_frames_array(frame_faces_pairs)
+    print(frames_array)
     # faces_array = convert_to_faces_array(frame_faces_pairs)
-
-    faces = face_extractor.extract(frame)
-    for face in faces:
-        face = asdict(face)
-        del face["alignedImage"]
-        del face["facenet512"]
-        print(face)
 
     return {
         "service": service,
