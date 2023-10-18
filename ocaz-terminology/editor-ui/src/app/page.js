1650ed8b8596179e16d070715dbb98b4f939dd2a
kleamp1e <kleamp1e@gmail.com>
2023-10-18 15:39:19 +0900

    用語の一覧を表示する処理を追加

--- ocaz-terminology/editor-ui/src/app/page.js
+++ ocaz-terminology/editor-ui/src/app/page.js
@@ -1,8 +1,29 @@
-"use client"
+"use client";
 
 import useSWR from "swr";
+import strftime from "strftime";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
+
+function TermTable({ terms }) {
+  return (
+    <table>
+      <tbody>
+        {terms.map((term) => (
+          <tr key={term.id}>
+            <td className="border">
+              {strftime("%Y-%m-%dT%H:%M:%S", new Date(term.updatedAt * 1000))}
+            </td>
+            <td className="border">{term.id}</td>
+            <td className="border">{term.parentId ?? "-"}</td>
+            <td className="border">{term.representatives?.ja ?? "-"}</td>
+            <td className="border">{term.representatives?.en ?? "-"}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
 
 export default function Page() {
   const { data, error, isLoading } = useSWR(
@@ -10,5 +31,11 @@ export default function Page() {
     fetcher
   );
   console.log({ data });
-  return <></>;
+  if (data == null) return <div>Loading...</div>;
+
+  return (
+    <>
+      <TermTable terms={data.terms} />
+    </>
+  );
 }
