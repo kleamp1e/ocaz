4a6c8e57ccc5ec02e64b54b80b30eb34d6e4b7db
kleamp1e <kleamp1e@gmail.com>
2023-10-18 15:29:39 +0900

    データを読み込む処理を追加

--- ocaz-terminology/editor-ui/src/app/page.js
+++ ocaz-terminology/editor-ui/src/app/page.js
@@ -1,3 +1,14 @@
+"use client"
+
+import useSWR from "swr";
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
 export default function Page() {
+  const { data, error, isLoading } = useSWR(
+    "http://localhost:8000/terms",
+    fetcher
+  );
+  console.log({ data });
   return <></>;
 }
