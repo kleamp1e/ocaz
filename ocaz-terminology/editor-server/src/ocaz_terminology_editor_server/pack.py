05b540937ffe1e7f5038cb0f314137be82b7d66b
kleamp1e <kleamp1e@gmail.com>
2023-11-04 11:21:00 +0900

    ID毎のテーブルを生成する処理を追加

--- ocaz-terminology/editor-server/src/ocaz_terminology_editor_server/pack.py
+++ ocaz-terminology/editor-server/src/ocaz_terminology_editor_server/pack.py
@@ -1,7 +1,30 @@
+import json
 import os
 from pathlib import Path
 
 import click
+
+
+def load_jsonl(path: Path) -> list[any]:
+    with path.open("r") as f:
+        return [json.loads(line) for line in f.readlines()]
+
+
+def load_fragments(dir_path: Path) -> list[any]:
+    fragments = []
+    for jsonl_path in sorted(dir_path.glob("*.jsonl")):
+        fragments.extend(load_jsonl(jsonl_path))
+    return fragments
+
+
+def make_id_records_table(records: list[any]) -> dict[list[any]]:
+    table = {}
+    for record in records:
+        id = record["id"]
+        items = table.get(id, [])
+        items.append(record)
+        table[id] = items
+    return table
 
 
 @click.command()
@@ -15,6 +38,16 @@ import click
 def main(data_dir: str) -> None:
     data_dir_path = Path(data_dir).resolve()
     term_dir_path = data_dir_path / "term"
+    fragment_dir_path = term_dir_path / "fragment"
+
+    records = load_fragments(fragment_dir_path)
+    # print(records)
+
+    table = make_id_records_table(records)
+    # print(table)
+    for id, records in table.items():
+        if len(records) >= 2:
+            print((id, records))
 
 
 if __name__ == "__main__":
